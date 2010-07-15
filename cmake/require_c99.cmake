17d3cb056b380daa8873090099e31476a64d0b60
Anton Novikov <an146@ya.ru>
2010-07-15 17:36:00 +0400

    formatting

--- cmake/require_c99.cmake
+++ cmake/require_c99.cmake
@@ -2,12 +2,12 @@ macro (require_c99)
 	# enabling c99 in gcc
 	string (REGEX MATCH "gcc(\\.exe)?$" GCC "${CMAKE_C_COMPILER}")
 	if (GCC)
-		set(CMAKE_C_FLAGS "-std=c99")
-	endif()
+		set (CMAKE_C_FLAGS "-std=c99")
+	endif ()
 
 	if ("${C99_SUPPORTED}" STREQUAL "")
 		message (STATUS "Checking for C99 support")
-		file(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/test_c99.c
+		file (WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/test_c99.c
 			"#if !defined(__STDC_VERSION__)\n"
 			"#error __STDC_VERSION__ must be defined\n"
 			"#endif\n"
@@ -16,12 +16,12 @@ macro (require_c99)
 			"#endif\n"
 			"int main()\n"
 			"{return 0;}\n")
-		try_compile(C99_SUPPORTED ${CMAKE_BINARY_DIR}
+		try_compile (C99_SUPPORTED ${CMAKE_BINARY_DIR}
 			${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/test_c99.c) 
-	endif()
+	endif ()
 	if (C99_SUPPORTED)
 		message (STATUS "C99 supported")
-	else()
+	else ()
 		message (FATAL_ERROR "C99 not supported")
 	endif ()
 endmacro ()
